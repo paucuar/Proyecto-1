6e5f62ee9d2dd87a739a753e14c38d012da43b9a
paucuar <paucualla@gmail.com>
2026-08-16 19:39:59 +0200

    Se ha arreglado un problema de seguridad

--- exportar_geojson.py
+++ exportar_geojson.py
@@ -1,22 +1,28 @@
-import psycopg
+import os
 import json
+import psycopg
+from dotenv import load_dotenv
 
+# 1. Cargar las variables secretas del archivo .env
+load_dotenv()
+
+# 2. Configurar la conexión leyendo de las variables de entorno
 DB_CONFIG = {
-    "dbname": "postgres",
-    "user": "postgres",
-    "password": "weaver",
-    "host": "localhost",
-    "port": "5432"
+    "dbname": os.getenv("DB_NAME", "postgres"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": int(os.getenv("DB_PORT", 5432))
 }
 
 def obtener_sensores_geojson():
     with psycopg.connect(**DB_CONFIG) as conn:
         with conn.cursor() as cur:
             query = """
-                SELECT
-                    id,
-                    nombre,
-                    bateria_ptc,
+                SELECT 
+                    id, 
+                    nombre, 
+                    bateria_pct, 
                     activa,
                     ST_AsGeoJSON(geom) AS geometria_geojson
                 FROM estaciones_sensores;
@@ -30,24 +36,24 @@ def obtener_sensores_geojson():
                 feature = {
                     "type": "Feature",
                     "geometry": json.loads(geom_json_str),
-                    "properties":{
+                    "properties": {
                         "id": id_sensor,
                         "nombre": nombre,
-                        "bateria_ptc": bateria,
+                        "bateria_pct": bateria,
                         "activa": activa
                     }
                 }
                 features.append(feature)
 
-            geojson_final = {
+            return {
                 "type": "FeatureCollection",
                 "features": features
             }
 
-            return geojson_final
-
 if __name__ == "__main__":
     datos_espaciales = obtener_sensores_geojson()
-    with open ("sensores.geojson", "w", encoding="utf-8") as f:
+    
+    with open("sensores.geojson", "w", encoding="utf-8") as f:
         json.dump(datos_espaciales, f, indent=4, ensure_ascii=False)
-    print("Archivo 'sensores.geojson generado correctamente.")
+        
+    print("Se ha generado el archivo de forma correcta")
